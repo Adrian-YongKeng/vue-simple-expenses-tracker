f5c9e1ce51b7880438e9c408e32f556b28595c68
Adrian <yy_1123@hotmail.com>
2025-06-11 15:46:26 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,12 @@
 
 <script setup>
 import { computed, onMounted, ref } from "vue";
+import { useToast } from "vue-toastification";
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
-import { useToast } from "vue-toastification";
 
 const toast = useToast();
 const transactions = ref([]);
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -4,6 +4,7 @@
       <h4>Income</h4>
       <p id="money-plus" class="money plus">+$ {{ income }}</p>
     </div>
+
     <div>
       <h4>Expense</h4>
       <p id="money-minus" class="money minus">$ {{ expenses }}</p>
